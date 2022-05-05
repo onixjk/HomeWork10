678bb8c18030c89e5bde13a0aeeb7ecbe6beb104
onixjk <onixjkk@gmail.com>
2022-05-05 10:46:16 +0300

    Initial Commit

---
+++ HomeWork10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork10
+//
+//  Created by Onix qq on 5.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
